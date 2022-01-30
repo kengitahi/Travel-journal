0e22b50034213846dd506f5a7842866468203d6e
Kennedy Gitahi <kengitahi@hotmail.com>
2022-01-30 12:59:46 +0300

    Complete Nav section

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav>
+            <img src="./assets/img/logo.png" alt="" className="nav__logo" />
+            <h1 className="nav__heading">
+                my travel journal.
+            </h1>
+        </nav>
+    )
+}
+
+export default Nav
